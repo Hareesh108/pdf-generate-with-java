b3b2f3983065b0af64df512da81845f005ea6f78
hareesh <hareeshbhittam@gmail.com>
2024-06-20 15:12:44 +0530

    Initial commit

---
+++ src/test/java/com/example/guarantor/GuarantorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guarantor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuarantorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
