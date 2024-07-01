e70d3c521b11a0da7d430acd1624854fbe710047
hareesh <hareeshbhittam@gmail.com>
2024-07-01 09:46:14 +0530

    Minor changes

--- src/main/java/com/example/guarantor/controller/PdfController.java
+++ src/main/java/com/example/guarantor/controller/PdfController.java
@@ -45,13 +45,10 @@ public class PdfController {
 
 		String renderedHtml = thymeleafService.renderTemplate("demo.xhtml", model);
 
-		System.out.println(renderedHtml);
-
 		PdfRendererBuilder pdfRendererBuilder = new PdfRendererBuilder();
 		pdfRendererBuilder.withHtmlContent(renderedHtml, "classpath:/static/");
 		try (OutputStream outputStream = new FileOutputStream("test.pdf")) {
 			pdfRendererBuilder.toStream(outputStream);
-			System.out.println(pdfRendererBuilder.toStream(outputStream) + "nnnn");
 			pdfRendererBuilder.run();
 		} catch (Exception e) {
 			System.out.println(e);
