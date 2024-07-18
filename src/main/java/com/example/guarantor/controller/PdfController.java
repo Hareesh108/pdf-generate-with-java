9237417571b0f9b69536adb6f9268c1ae73dfa65
hareesh <hareeshbhittam@gmail.com>
2024-07-18 18:03:00 +0530

    Added header for the pdf

--- src/main/java/com/example/guarantor/controller/PdfController.java
+++ src/main/java/com/example/guarantor/controller/PdfController.java
@@ -43,7 +43,7 @@ public class PdfController {
 		Map<String, Object> model = new HashMap<>();
 		model.put("keyValues", keyValues);
 
-		String renderedHtml = thymeleafService.renderTemplate("reminder-cl03.xhtml", model);
+		String renderedHtml = thymeleafService.renderTemplate("call-card.xhtml", model);
 
 		PdfRendererBuilder pdfRendererBuilder = new PdfRendererBuilder();
 		pdfRendererBuilder.withHtmlContent(renderedHtml, "classpath:/static/");
